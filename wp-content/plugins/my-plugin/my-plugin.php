a51047c1953f1856f29f6f4c07929d6769ca9b17
tmpard <triciapardave@gmail.com>
2019-03-05 17:29:35 -0500

    Task: added sample code to my-plugin.php

--- wp-content/plugins/my-plugin/my-plugin.php
+++ wp-content/plugins/my-plugin/my-plugin.php
@@ -5,3 +5,12 @@
  * Date: 3/5/2019
  * Time: 5:20 PM
  */
+
+function dh_modify_read_more_link() {
+
+    return '<a class="more-link" href="' . get_permalink() . '">Click to Read!</a>';
+
+}
+
+add_filter( 'the_content_more_link', 'dh_modify_read_more_link' );
+
