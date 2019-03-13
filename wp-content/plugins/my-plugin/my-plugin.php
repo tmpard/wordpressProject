4b1bea55cb3cc9d3acad37eedc3277c8107d5abc
tmpard <triciapardave@gmail.com>
2019-03-13 11:53:48 -0400

    Task: Added "add_filter" function to get plugin working

--- wp-content/plugins/my-plugin/my-plugin.php
+++ wp-content/plugins/my-plugin/my-plugin.php
@@ -14,3 +14,5 @@ function dh_modify_read_more_link() {
     return '<a class="more-link" href="' . get_permalink() . '">Click to Read!</a>';
 
 }
+
+add_filter( 'the_content_more_link', 'dh_modify_read_more_link' );
