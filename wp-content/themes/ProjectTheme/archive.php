9ed01fb57aecfdcbc58d40ec8be98cb7d650585b
Peter <phe3@njit.edu>
2019-03-20 22:37:13 -0400

    adjusting archives

--- wp-content/themes/ProjectTheme/archive.php
+++ wp-content/themes/ProjectTheme/archive.php
@@ -3,8 +3,9 @@
 <div class="container">
     <h1><?php single_cat_title();?></h1>
     <?php if(have_posts()) : while(have_posts()) : the_post();?>
+        <h3><?php the_title();?></h3>
         <?php the_excerpt();?>
-    <?php endwhile; endif; ?>
+    <?php endwhile; endif;?>
 </div>
 
 <?php get_footer();?>
