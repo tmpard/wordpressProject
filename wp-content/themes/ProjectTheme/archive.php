928a394fb622487c1be7079b3a62d7245d4bfa11
Peter <phe3@njit.edu>
2019-03-20 22:35:46 -0400

    adjusting archives

--- wp-content/themes/ProjectTheme/archive.php
+++ wp-content/themes/ProjectTheme/archive.php
@@ -1,7 +1,10 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Peter
- * Date: 2019-03-20
- * Time: 19:10
- */
+<?php get_header();?>
+
+<div class="container">
+    <h1><?php single_cat_title();?></h1>
+    <?php if(have_posts()) : while(have_posts()) : the_post();?>
+        <?php the_excerpt();?>
+    <?php endwhile; endif; ?>
+</div>
+
+<?php get_footer();?>
