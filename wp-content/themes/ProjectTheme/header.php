e0f4a633fe872981e1a0ae35c9dd0e2da7106425
Peter <phe3@njit.edu>
2019-03-20 22:34:21 -0400

    troubleshooting

--- wp-content/themes/ProjectTheme/header.php
+++ wp-content/themes/ProjectTheme/header.php
@@ -8,5 +8,15 @@
 <body <?php body_class();?>>
 
 <header class="sticky-top">
+    <div class="container">
+    <?php wp_nav_menu(
+
+            array(
+
+                    'theme_location' => 'top-menu',
+                    'menu_class' => 'navigation'
+            )
+);?>
+    </div>
 
 </header>
